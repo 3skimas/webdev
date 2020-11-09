9c19badbb3e6a225d061f0b47cb11fbd57b272aa
3skimas <33157424+3skimas@users.noreply.github.com>
2020-11-09 09:11:14 +0100

    Assigment JS.1.2[for loop fix]

--- Assignment JS.1.2/number.js
+++ Assignment JS.1.2/number.js
@@ -10,21 +10,15 @@ buttonas.onclick = function(){
   let random4 = Math.floor(1 + Math.random() * 6);
   let random5 = Math.floor(1 + Math.random() * 6);
 
-  let number1 = document.getElementById('number1');
-  let number2 = document.getElementById('number2');
-  let number3 = document.getElementById('number3');
-  let number4 = document.getElementById('number4');
-  let number5 = document.getElementById('number5');
+  let number = document.getElementsByTagName('span');
 
   let dice = [random1, random2, random3, random4, random5];
-  console.log(dice);
 
-  number1.innerHTML = dice[0];
-  number2.innerHTML = dice[1];
-  number3.innerHTML = dice[2];
-  number4.innerHTML = dice[3];
-  number5.innerHTML = dice[4];
-
+  var i;
+  for (i = 0; i < number.length; i++) {
+    number[i].innerHTML = dice[i];
+    console.log(dice[i]);
+  }
 }
 
 
