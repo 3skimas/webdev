6b1e9ade37c47fd42e8e205e7c3ef32e732a37b8
3skimas <33157424+3skimas@users.noreply.github.com>
2020-11-27 10:14:59 +0100

    scenario

--- scenario_1/js/booking.js
+++ scenario_1/js/booking.js
@@ -1,13 +1,43 @@
 'use strict';
-const $ = function (foo) { return document.getElementById(foo); }
+let button = document.getElementById('btn');
+let counter = 1;
+let storedSettings;
 
-const isLocalStorageEnabled = function () {
-    let foo = 'bar';
-    try {
-        localStorage.setItem(foo, foo);
-        localStorage.removeItem(foo);
-        return true;
-    } catch(e) {
-        return false;
+
+
+const writedata = function () {
+
+    let name = document.getElementById('name').value;
+    let surename = document.getElementById('surename').value;
+    let date = document.getElementById('datetime').value;
+
+    const reservation = {
+            id: counter,
+            name: name,
+            surename: surename,
+            date: date
     }
+    localStorage.setItem('reservation' + counter, JSON.stringify(reservation));
 }
+
+const getdata = function () {
+    for(let i = 0; i < localStorage.length; i++){
+        storedSettings = JSON.parse(localStorage.getItem('reservation' + i));
+    }
+    document.getElementById('result').innerHTML = 'First Name ' + storedSettings.id;
+
+}
+
+
+
+window.onload = function () {
+    counter = localStorage.length;
+}
+
+const run = function(){
+    writedata();
+    console.log(localStorage.getItem('reservation' + counter));
+    counter++;
+    
+}
+button.addEventListener('click',run);
